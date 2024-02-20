56dd25c2a6d5ce0dba80ea276c1f513bce865957
cristian <cristianalvessilvak@gmail.com>
2024-02-20 01:14:33 -0300

    Remold menu

--- main.sh
+++ main.sh
@@ -1,16 +1,24 @@
-#!/bin/bash
+#!/bin/bash 
+
+RESET="\e[0m"
+RED="\e[91m"
+GREEN="\e[92m"
+YELLOW="\e[93m"
+CYAN="\e[96m"
+BOLD="\e[1m"
+BLUE="\e[34m"
+MAGENTA="\e[35m"
+PISCAR="\e[5m"
 
 if [ "$(id -u)" -ne 0 ]; then
 	echo "Por favor, execute este script como root"
 	exit 1
 fi
 
-echo "|=================================================|"
-echo "|			MENU PRINCIPAL			|"
-echo "|=================================================|"
-echo "|							|"
-echo "|	[1] - Docker					|"
-echo "|	[2] - SSH					|"
-echo "| [3] - Samba					|"
-echo "| [4] - VSFTPD					|"
-echo "|_________________________________________________|"
+echo "{$CYAN}╔═══════════════════════════╗"
+echo "║{$RESET}              MENU PRINCIPAL         {$CYAN}║"
+echo "╠═══════════════════════════╣"
+echo "║{$RESET} [1] - Docker                        {$CYAN}║"
+echo "║{$RESET} [2] - Serviços                      {$CYAN}║"
+echo "║{$RESET} [0] - Sair                          {$CYAN}║"
+echo "╚═══════════════════════════╝{$RESET}"
