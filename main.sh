fc95c4770a5fbb44643620b313096871eeb0b651
Cristian A. Silva <cristianalvessilvak@gmail.com>
2024-05-23 14:27:32 -0300

    Novamente o new.sh

--- main.sh
+++ main.sh
@@ -330,64 +330,114 @@ container_create() {
 # ....................................................................................
 # .........................::: MANIPULAÇÃO DO DOCKER :::.........................
 
-docker_install() {
-    clear
+function docker_install() {
+    echo -e "${NEWLINE}${NEWLINE}"
     docker --version
-    if [ $? -eq 0 ]; then
-        echo "Docker ja está instalado!"
-    else
-        clear
-        echo "...::: Instalando Docker :::..."
-        echo "Atualizando o sistema..."
+    if ! [ $? -eq 0 ]; then
+        echo -e "${NEWLINE}${NEWLINE}"
+        sleep 1
+        echo -e "${BLUE}-----------------------------------------${NC}"
+        echo -e "   ${BOLD}Instalação do Docker!${NC}     "
+        echo -e "${BLUE}-----------------------------------------${NC}"
+        sleep 1
+        echo -e "${NEWLINE}${NEWLINE}"
+
+        echo -e "${MAGENTA} ----- [${NC} Atualizando sistema ${MAGENTA}] -----${NC}${NEWLINE}"
+        sleep 1
         apt update && apt upgrade -y
         if [ $? -ne 0 ]; then
-            echo "Erro ao atualizar o sistema. Verifique sua conexão com a internet e tente novamente."
+            echo -e "${ERROR}<<< ERRO >>>:${NC} Erro ao atualizar o sistema. Verifique sua conexão com a internet e tente novamente.${NEWLINE}"
+            sleep 1
             return
         fi
-        clear
-        echo "Instalando pacotes necessários..."
+
+        echo -e "${MAGENTA} ----- [${NC} Instalando pacotes necessários ${MAGENTA}] -----${NC}${NEWLINE}"
+        sleep 1
         apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
         if [ $? -ne 0 ]; then
-            echo "Erro ao instalar pacotes necessários. Verifique sua conexão com a internet e tente novamente."
-            return
-        fi
-        clear
-        echo "Adicionando chave GPG do repositório Docker..."
-        curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-        if [ $? -ne 0 ]; then
-            echo "Erro ao adicionar chave GPG do repositório Docker. Verifique sua conexão com a internet e tente novamente."
+            echo -e "${ERROR}<<< ERRO >>>:${NC} Erro ao instalar pacotes necessários. Verifique sua conexão com a internet e tente novamente.${NEWLINE}"
+            sleep 1
             return
         fi
 
-        clear
-        echo "Adicionando repositório Docker ao sistema..."
-        echo "deb [signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list >/dev/null
-        apt update
-        clear
-        echo "Instalando Docker Engine..."
-        apt install -y docker-ce docker-ce-cli containerd.io
-        if [ $? -ne 0 ]; then
-            echo "Erro ao instalar Docker Engine. Verifique sua conexão com a internet e tente novamente."
+        echo -e "${MAGENTA} ----- [${NC} Adicionando chave GPG do repositório Docker ${MAGENTA}] -----${NC}${NEWLINE}"
+        sleep 1
+        curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+        if [ $? -ne 0 ]; então
+            echo -e "${ERROR}<<< ERRO >>>:${NC} Erro ao adicionar chave GPG no repositório Docker. Verifique sua conexão com a internet e tente novamente.${NEWLINE}"
+            sleep 1
             return
         fi
-        clear
-        echo "Adicionando usuário ao grupo docker..."
-        echo -e "${BOLD} Nome do usuário que ira utilizar o Docker: ${RESET}"
+
+        echo -e "${MAGENTA} ----- [${NC} Adicionando repositório Docker ao sistema ${MAGENTA}] -----${NC}${NEWLINE}"
+        sleep 1
+        echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+        apt update
+        sleep 1
+
+        echo -e "${MAGENTA} ----- [${NC} Instalando Docker Engine ${MAGENTA}] -----${NC}${NEWLINE}"
+        sleep 1
+        apt install -y docker-ce docker-ce-cli containerd.io
+        if [ $? -ne 0 ]; então
+            echo -e "${ERROR}<<< ERRO >>>:${NC} Erro ao instalar Docker Engine. Verifique sua conexão com a internet e tente novamente.${NEWLINE}"
+            sleep 1
+            return
+        fi
+
+        echo -e "${MAGENTA} ----- [${NC} Adicionando usuário ao grupo docker ${MAGENTA}] -----${NC}${NEWLINE}"
+        sleep 1
+
+        /etc/init.d/docker restart
+        if [ $? -ne 0 ]; então
+            echo -e "${ERROR}<<< ERRO >>>:${NC} Erro ao reiniciar o serviço Docker."
+            return
+        fi
+        sleep 2
+
+        echo -ne "${GREEN}${BLINK} ->${NC} Nome do usuário que irá utilizar o Docker: "
         read usr
         usermod -aG docker $usr
-        chown $usr:docker /var/run/docker.sock
-        /etc/init.d/docker restart
-        clear
-        docker --version
-        if [ $? -eq 0 ]; then
-            echo "Docker foi instalado com sucesso!"
-        else
-            echo "Erro: Docker não está instalado ou ocorreu um problema durante a verificação."
+        if [ $? -ne 0 ]; então
+            echo -e "${ERROR}<<< ERRO >>>:${NC} Erro ao adicionar usuário: ${usr} ao grupo docker."
+            return
         fi
 
-    fi
+        # Mudança de permissão do socket Docker
+        if [ -e /var/run/docker.sock ]; então
+            chown $usr:docker /var/run/docker.sock
+            chmod 660 /var/run/docker.sock
+            if [ $? -ne 0 ]; então
+                echo -e "${ERROR}<<< ERRO >>>: ${NC} Erro ao mudar permissão do arquivo /var/run/docker.sock"
+                return
+            fi
+        else
+            echo -e "${ERROR}<<< ERRO >>>:${NC} O arquivo /var/run/docker.sock não existe."
+            return
+        fi
 
+        /etc/init.d/docker restart
+        sleep 1
+
+        docker --version
+        if [ $? -eq 0 ]; então
+            echo -e "${SUCCESS}.........................................${NC}"
+            echo -e "${BOLD} ...::: Docker instalado com sucesso! :::... ${NC}"
+            echo -e "${SUCCESS}.........................................${NC}${NEWLINE}"
+            echo -e "${BOLD} ...::: Por favor, faça logout e login novamente para aplicar as mudanças de grupo. :::... ${NC}"
+            sleep 1
+        else
+            echo -e "${ERROR}<<< ERRO >>>:${NC} Erro ao instalar Docker Engine. Verifique sua conexão com a internet e tente novamente.${NEWLINE}"
+            sleep 1
+        fi
+    else
+        echo -e "${NEWLINE}${NEWLINE}"
+        echo -e "${SUCCESS}.........................................${NC}"
+        echo -e "${BOLD} ...::: Docker já está instalado! :::... ${NC}"
+        echo -e "${SUCCESS}.........................................${NC}"
+        echo -e "${NEWLINE}${NEWLINE}"
+    fi
 }
+
 docker_uninstall() {
     clear
     docker --version
