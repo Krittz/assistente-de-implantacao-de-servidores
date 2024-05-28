ed3da60e91a084531b5f2f4ab6ba334d4b602e30
Cristian A. Silva <cristianalvessilvak@gmail.com>
2024-05-27 23:15:48 -0300

    Adicionando opção de bancos de dados, e implementando o banco MySQL

--- new.sh
+++ new.sh
@@ -12,18 +12,16 @@ BOLD='\033[1m'
 NEWLINE='\n'
 BLINK='\033[5m'
 
-# Função para verificar se um contêiner com o mesmo nome já está em uso
 function container_name_in_use(){
     container_name=$1
     if [ ! -z "$(docker ps -a --filter name=^/${container_name}$ --format '{{.Names}}')" ]; then
-        echo -e "${ERROR}<<< ERRO >>> :${NC} Nome de contêiner '${container_name}' já está em uso. Por favor, escolha outro nome."
+        echo -e "${ERROR}<<< ${BOLD}ERRO${NC} >>> :${NC} Nome de contêiner '${BOLD}${container_name}${NC}' já está em uso. Por favor, escolha outro nome."
         return 1
     else
         return 0
     fi
 }
 
-# Função para verificar portas disponíveis
 function list_available_ports(){
     start_port=$1
     end_port=$2
@@ -36,21 +34,20 @@ function list_available_ports(){
     echo "${available_ports[@]}"
 }
 
-# Função para escolher a porta
 function choose_port(){
     start_port=$1
     end_port=$2
     echo -e "${BLINK}${GREEN}->${NC}Portas disponíveis entre ${start_port} e ${end_port}:"
     available_ports=$(list_available_ports $start_port $end_port)
     if [ -z "$available_ports" ]; then
-        echo -e "${ERROR}<<< ERRO >>> :${NC} Nenhuma porta disponível encontrada entre ${start_port} e ${end_port}."
+        echo -e "${ERROR}<<< ${BOLD}ERRO${NC} >>> :${NC} Nenhuma porta disponível encontrada entre ${start_port} e ${end_port}."
         return 1
     fi
     echo -e "${available_ports}"
     echo -ne "${BLINK}${GREEN}->${NC}Escolha uma porta: "
     read -r chosen_port
     if [[ ! " ${available_ports[@]} " =~ " ${chosen_port} " ]]; then
-        echo -e "${ERROR}<<< ERRO >>> :${NC} Porta escolhida não está disponível. Por favor, escolha outra porta."
+        echo -e "${ERROR}<<< ${BOLD}ERRO${NC} >>> :${NC} Porta escolhida não está disponível. Por favor, escolha outra porta."
         return 1
     fi
     echo $chosen_port
@@ -58,14 +55,120 @@ function choose_port(){
 }
 
 # ------------------------------------------------------------------------------------
+# .................................. BANCO DE DADOS ..................................
+# ------------------------------------------------------------------------------------
+function setup_mysql(){
+    echo -e "${NEWLINE}${MAGENTA} ----- [${NC} Configurando MySQL ${MAGENTA}] -----${NC}${NEWLINE}"
+    
+    echo -ne "${BLINK}${GREEN}->${NC} Digite o nome do container: "
+    read -r container_name
+    if [ -z "$container_name" ]; then
+        echo -e "${ERROR}<<< ${BOLD}ERRO${NC} >>> :${NC} Nome do container não pode ser vazio."
+        sleep 1
+        setup_mysql
+        return
+    fi
+    if container_name_in_use $container_name; then
+        echo -e "${ERROR}<<< ${BOLD}ERRO${NC} >>>:${NC} Nome do container já está em uso."
+        sleep 1
+        setup_mysql
+        return
+    fi
+
+    echo -ne "${BLINK}${GREEN}->${NC} Digite a senha do usuário root: "
+    read -r root_password
+    if [ -z "$root_password" ]; then
+        echo -e "${ERROR}<<< ${BOLD}ERRO${NC} >>> :${NC} Senha do usuário root não pode ser vazia."
+        sleep 1
+        setup_mysql
+        return
+    fi
+
+    mysql_port=$(choose_port 3306 3350)
+    if [ $? -ne 0 ]; then
+        sleep 1
+        setup_mysql
+        return
+    fi
+
+    echo -ne "${BLINK}${GREEN}->${NC} Digite o caminho para o arquivo de backup do banco de dados (opcional): "
+    read -r backup_file
+
+    mkdir -p configs/database/mysql
+    cat <<EOF > configs/database/mysql/docker-compose.yml
+version: '3'
+services:
+    ${container_name}:
+        image: mysql:latest
+        environment:
+            MYSQL_ROOT_PASSWORD: ${root_password}
+        ports:
+            - "${mysql_port}:3306"
+        volumes:
+            - mysql_data:/var/lib/mysql
+EOF
+    if [ -n "$backup_file" ]; then
+        cat <<EOF >> configs/database/mysql/docker-compose.yml
+            - ${backup_file}:/docker-entrypoint-initdb.d/backup.sql
+EOF
+    fi
+
+    cat <<EOF >> configs/database/mysql/docker-compose.yml
+volumes:
+    mysql_data:
+EOF
+
+    sleep 1
+    docker-compose -f configs/database/mysql/docker-compose.yml up -d
+    sleep 1
+    echo -e "${SUCCESS}+ ${BOLD}ÊXITO${NC} + :${NC} Banco de dados MySQL configurado com sucesso no container ${BOLD}${container_name}${NC} na porta: ${BOLD}${mysql_port}${NC}!"
+}
+
+function configure_databases(){
+    echo -e "${NEWLINE}${BLUE}Escolha a destinação do servidor de banco de dados:${NC}"
+    echo -e "[${GREEN}1${NC}] . Banco MySQL"
+    echo -e "[${GREEN}2${NC}] . Banco MariaDB"
+    echo -e "[${GREEN}3${NC}] . Banco PostgreSQL"
+    echo -e "[${GREEN}0${NC}] . Voltar"
+    echo -ne "${BLINK}${GREEN}->${NC}Escolha uma opção: "
+    read destination_choice
+        case $destination_choice in
+        1)
+            setup_mysql
+            sleep 1
+            ;;
+        2) 
+            ##setup_mariadb
+            sleep 1
+            ;;
+        3) 
+            #setup_postgresql
+            sleep 1
+            ;;
+        0)
+            return
+            ;;
+        *)
+            echo -e "${WARNING} * ${BOLD}AVISO${NC} * :${NC} Opção inválida...${NC}"
+            sleep 1
+            configure_databases
+            ;;
+    esac  
+}
+
+# ------------------------------------------------------------------------------------
+# .................................. //BANCO DE DADOS ................................
+# ------------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------------
 # .................................. APACHE ..........................................
 # ------------------------------------------------------------------------------------
+
 function setup_static_site(){
-    echo -e "${NEWLINE}${MAGENTA} ----- [${NC} Configurando Apache para hospedar site estático ${MAGENTA}] -----${NC}${NEWLINE}"
     echo -ne "${BLINK}${GREEN}->${NC}Digite o caminho para os arquivos do site: "
     read -r static_site_path
     if [ -z "$static_site_path" ]; then
-        echo -e "${ERROR}<<< ERRO >>> :${NC} Caminho não pode ser vazio."
+        echo -e "${ERROR}<<< ${BOLD}ERRO${NC} >>> :${NC} Caminho não pode ser vazio."
         sleep 1
         setup_static_site
         return
@@ -73,7 +176,7 @@ function setup_static_site(){
     echo -ne "${BLINK}${GREEN}->${NC}Digite o nome do contêiner: "
     read -r container_name
     if [ -z "$container_name" ]; then
-        echo -e "${ERROR}<<< ERRO >>> :${NC} Nome do contêiner não pode ser vazio."
+        echo -e "${ERROR}<<< ${BOLD}ERRO${NC} >>> :${NC} Nome do contêiner não pode ser vazio."
         sleep 1
         setup_static_site
         return
@@ -103,61 +206,7 @@ EOF
     sleep 1
     docker-compose -f configs/apache/static_site/docker-compose.yml up -d
     sleep 1
-    echo -e "${SUCCESS}+ ÊXITO + :${NC} Site estático hospedado com sucesso no contêiner '${container_name}' na porta ${static_site_port}!"
-}
-
-function setup_dynamic_site(){
-    echo -e "${NEWLINE}${MAGENTA} ----- [${NC} Configurando Apache para hospedar site dinâmico ${MAGENTA}] -----${NC}${NEWLINE}"
-    echo -ne "${BLINK}${GREEN}->${NC}Digite o caminho para os arquivos do site dinâmico: "
-    read -r dynamic_site_path
-    if [ -z "$dynamic_site_path" ]; then
-        echo -e "${ERROR}<<< ERRO >>> :${NC} Caminho não pode ser vazio."
-        sleep 1
-        setup_dynamic_site
-        return
-    fi
-    echo -ne "${BLINK}${GREEN}->${NC}Digite a versão do PHP desejada (ex: 7.4, 8.0, 8.1): "
-    read -r php_version
-    if [ -z "$php_version" ]; then
-        echo -e "${ERROR}<<< ERRO >>> :${NC} Versão do PHP não pode ser vazia."
-        sleep 1
-        setup_dynamic_site
-        return
-    fi
-    echo -ne "${BLINK}${GREEN}->${NC}Digite o nome do contêiner: "
-    read -r container_name
-    if [ -z "$container_name" ]; then
-        echo -e "${ERROR}<<< ERRO >>> :${NC} Nome do contêiner não pode ser vazio."
-        sleep 1
-        setup_dynamic_site
-        return
-    fi
-    if container_name_in_use $container_name; then
-        sleep 1
-        setup_dynamic_site
-        return
-    fi
-    dynamic_site_port=$(choose_port 8080 8090)
-    if [ $? -ne 0 ]; then
-        sleep 1
-        setup_dynamic_site
-        return
-    fi
-    mkdir -p configs/apache/dynamic_site
-    cat <<EOF > configs/apache/dynamic_site/docker-compose.yml
-version: '3'
-services:
-    ${container_name}:
-        image: php:${php_version}-apache
-        volumes:
-            - ${dynamic_site_path}:/var/www/html
-        ports:
-            - "${dynamic_site_port}:80"
-EOF
-    sleep 1
-    docker-compose -f configs/apache/dynamic_site/docker-compose.yml up -d
-    sleep 1
-    echo -e "${SUCCESS}+ ÊXITO + :${NC} Site dinâmico hospedado com sucesso no contêiner '${container_name}' na porta ${dynamic_site_port}!"
+    echo -e "${SUCCESS}+ ${BOLD}ÊXITO${NC} + :${NC} Site estático hospedado com sucesso no contêiner '${BOLD}${container_name}${NC}' na porta ${BOLD}${static_site_port}${NC}!"
 }
 
 function configure_apache_server(){
@@ -165,8 +214,7 @@ function configure_apache_server(){
     echo -e "[${GREEN}1${NC}] . Hospedar um site estático"
     echo -e "[${GREEN}2${NC}] . Hospedar um site dinâmico (PHP)"
     echo -e "[${GREEN}3${NC}] . Hospedar uma API"
-    echo -e "[${GREEN}4${NC}] . Hospedar um banco de dados MySQL"
-    echo -e "[${GREEN}5${NC}] . Hospedar um banco de dados MariaDB"
+    echo -e "[${GREEN}4${NC}] . Configurar Banco de Dados"
     echo -e "[${GREEN}0${NC}] . Voltar"
     echo -ne "${BLINK}${GREEN}->${NC}Escolha uma opção: "
     read destination_choice
@@ -184,18 +232,14 @@ function configure_apache_server(){
             sleep 1
             ;;
         4)
-            #setup_mysql
-            sleep 1
-            ;;
-        5)
-            #setup_mariadb
+            configure_databases
             sleep 1
             ;;
         0)
             return
             ;;
         *)
-            echo -e "${WARNING} * AVISO * :${NC} Opção inválida...${NC}"
+            echo -e "${WARNING} * ${BOLD}AVISO${NC} * :${NC} Opção inválida...${NC}"
             sleep 1
             configure_apache_server
             ;;
@@ -213,12 +257,10 @@ function docker_compose_install(){
     echo -e "${NEWLINE}${MAGENTA} ----- [${NC} Instalando Docker Compose ${MAGENTA}] -----${NC}${NEWLINE}"
     apt install -y docker-compose
     if [ $? -eq 0 ]; then
-        echo -e "${NEWLINE}${SUCCESS}.........................................${NC}"
-        echo -e "${BOLD} ...::: Compose instalado com sucesso! :::... ${NC}"
-        echo -e "${SUCCESS}.........................................${NC}${NEWLINE}"
+        echo -e "${NEWLINE}${SUCCESS}+ ${BOLD}ÊXITO${NC} + :${NC} Docker Compose instalado com sucesso!"
         sleep 1
     else
-        echo -e "${NEWLINE}${ERROR}<<< ERRO >>>:${NC} Erro ao instalar Docker Compose. Verifique sua conexão com a internet e tente novamente."
+        echo -e "${NEWLINE}${ERROR}<<< ${BOLD}ERRO${NC} >>>:${NC} Erro ao instalar Docker Compose. Verifique sua conexão com a internet e tente novamente."
         sleep 1
         return
     fi
@@ -240,7 +282,7 @@ function docker_install(){
         curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
         echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
         if [ $? -ne 0 ]; then
-            echo -e "${ERROR}<<< ERRO >>>:${NC} Erro ao adicionar repositório Docker ao sistema. Verifique sua conexão com a internet e tente novamente.${NEWLINE}"
+            echo -e "${ERROR}<<< ${BOLD}ERRO${NC} >>> :${NC} Erro ao adicionar repositório Docker ao sistema. Verifique sua conexão com a internet e tente novamente.${NEWLINE}"
             sleep 1
             return
         fi
@@ -248,7 +290,7 @@ function docker_install(){
         sleep 1
         apt update
         if [ $? -ne 0 ]; then
-            echo -e "${ERROR}<<< ERRO >>>:${NC} Erro ao atualizar índices do repositório Docker. Verifique sua conexão com a internet e tente novamente.${NEWLINE}"
+            echo -e "${ERROR}<<< ${BOLD}ERRO${NC} >>> :${NC} Erro ao atualizar índices do repositório Docker. Verifique sua conexão com a internet e tente novamente.${NEWLINE}"
             sleep 1
             return
         fi
@@ -256,17 +298,15 @@ function docker_install(){
         sleep 1
         apt install -y docker-ce docker-ce-cli containerd.io
         if [ $? -eq 0 ]; then
-            echo -e "${SUCCESS}.........................................${NC}"
-            echo -e "${BOLD} ...::: Docker instalado com sucesso! :::... ${NC}"
-            echo -e "${SUCCESS}.........................................${NC}${NEWLINE}"
+            echo -e "${SUCCESS}+ ${BOLD}ÊXITO${NC} + :${NC} Docker instalado com sucesso!"
             sleep 1
         else
-            echo -e "${ERROR}<<< ERRO >>>:${NC} Erro ao instalar Docker. Verifique sua conexão com a internet e tente novamente.${NEWLINE}"
+            echo -e "${ERROR}<<< ${BOLD}ERRO${NC} >>> :${NC} Erro ao instalar Docker. Verifique sua conexão com a internet e tente novamente.${NEWLINE}"
             sleep 1
             return
         fi
     else
-        echo -e "${WARNING} * AVISO * :${NC} Docker já está instalado.${NEWLINE}"
+        echo -e "${WARNING} * ${BOLD}AVISO${NC} * :${NC} Docker já está instalado.${NEWLINE}"
     fi
 }
 
@@ -284,6 +324,7 @@ function main_menu(){
     echo -e "[${GREEN}1${NC}] . Instalar Docker"
     echo -e "[${GREEN}2${NC}] . Instalar Docker Compose"
     echo -e "[${GREEN}3${NC}] . Configurar Servidor Apache"
+    echo -e "[${GREEN}4${NC}] . Configurar Banco de Dados"
     echo -e "[${GREEN}0${NC}] . Sair"
     echo -ne "${BLINK}${GREEN}->${NC}Escolha uma opção: "
     read main_choice
@@ -300,13 +341,17 @@ function main_menu(){
             configure_apache_server
             sleep 1
             ;;
+        4)
+            configure_databases
+            sleep 1
+            ;;
         0)
             echo -e "${NEWLINE}${WHITE}Saindo...${NC}"
             sleep 1
             exit
             ;;
         *)
-            echo -e "${WARNING} * AVISO * :${NC} Opção inválida...${NC}"
+            echo -e "${WARNING} * ${BOLD}AVISO${NC} * :${NC} Opção inválida...${NC}"
             sleep 1
             main_menu
             ;;
