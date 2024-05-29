2b069aeb372d035623c3384bb6ef6fbd0c1240ab
Cristian <cristianalvessilvak@gmail.com>
2024-05-28 21:30:45 -0300

    Corrigindo menu

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 ERROR='\033[0;31m'
-GREEN='\033[0;32m'
+INPUT='\033[0;32m'
 WARNING='\033[1;33m'
+SUCCESS='\033[0;36m'
 BLUE='\033[0;34m'
 MAGENTA='\033[0;35m'
-SUCCESS='\033[0;36m'
 WHITE='\033[1;37m'
 NC='\033[0m'
 BOLD='\033[1m'
@@ -12,20 +12,58 @@ NL='\n'
 BLINK='\033[5m'
 
 # --->>> MENUS <<<---
-function apache_menu(){}
-function nginx_menu(){}
-
-function servers_menus(){
+#function apache_menu(){}
+#function nginx_menu(){}
+#function samba_menu(){}
+#function proftpd_menu(){}
+#function vsftpd_menu(){}
+function server_menu(){
     echo -e "${NL}${BLUE}########################"
     echo -e "##     ${NC}${BOLD}SERVIDORES${NC}${BLUE}     ##"
     echo -e "##....................##"
-    echo -e "##${NC} [1] - Apache       ${BLUE}##"
-    echo -e "##${NC} [2] - NginX        ${BLUE}##"
-    echo -e "##${NC} [3] - Samba        ${BLUE}##"
-    echo -e "##${NC} [4] - ProFTPD      ${BLUE}##"
-    echo -e "##${NC} [5] - vsFPTD       ${BLUE}##"
+    echo -e "##${NC} [${INPUT}1${NC}] - Apache       ${BLUE}##"
+    echo -e "##${NC} [${INPUT}2${NC}] - NginX        ${BLUE}##"
+    echo -e "##${NC} [${INPUT}3${NC}] - Samba        ${BLUE}##"
+    echo -e "##${NC} [${INPUT}4${NC}] - ProFTPD      ${BLUE}##"
+    echo -e "##${NC} [${INPUT}5${NC}] - vsFTPD       ${BLUE}##"
     echo -e "########################${NC}"
-    
+    echo -ne "${BLINK}${INPUT}>>>${NC} Selecione uma opção: "
+    read -r server_option
+
+    case $server_option in
+        1)
+            sleep 0.3
+            echo "apache_menu"
+            ;;
+        2)
+            sleep 0.3
+           echo "nginx_menu"
+            ;;
+        3)
+            sleep 0.3
+            echo "samba_menu"
+            ;;
+        4)
+            sleep 0.3
+            echo "proftpd_menu"
+            ;;
+        5)
+            sleep 0.3
+            echo "vsftpd_menu"
+            ;;
+        0)
+            sleep 0.3
+            return
+            ;;
+        *)
+            sleep 0.3
+            echo -e "${WARNING}${BOLD}⚠ AVISO ⚠ ${NC}: Opção inválida!"
+            sleep 0.3
+            server_menu
+            ;;
+    esac
 }
 
-function main_menu(){}
+#function main_menu(){}
+
+server_menu
