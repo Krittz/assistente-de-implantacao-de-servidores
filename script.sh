2a15b2de5e1f6ee7b7ac7ae0823e7142b04a37b1
Cristian <cristianalvessilvak@gmail.com>
2024-05-28 21:38:56 -0300

    Adicionando menu de bancos de dados

--- script.sh
+++ script.sh
@@ -12,6 +12,49 @@ NL='\n'
 BLINK='\033[5m'
 
 # --->>> MENUS <<<---
+
+function database_menu(){
+    echo -e "${NL}${BLUE}#########################"
+    echo -e "##   ${NC}${BOLD}BANCOS DE DADOS${NC}${BLUE}   ##"
+    echo -e "##.....................##"
+    echo -e "##${NC} [${INPUT}1${NC}] - MySQL         ${BLUE}##"
+    echo -e "##${NC} [${INPUT}2${NC}] - MariaDB       ${BLUE}##"
+    echo -e "##${NC} [${INPUT}3${NC}] - PostgreSQL    ${BLUE}##"
+    echo -e "##${NC} [${INPUT}4${NC}] - SQLite        ${BLUE}##"
+    echo -e "##${NC} [${INPUT}0${NC}] - Voltar        ${BLUE}##"
+    echo -e "#########################${NC}"
+    echo -ne "${BLINK}${INPUT}>>>${NC} Selecione uma opção: "
+    read -r database_option
+    case $database_option in
+        1)
+            sleep 0.3
+            echo "mysql_menu"
+            ;;
+        2)
+            sleep 0.3
+           echo "mariadb_menu"
+            ;;
+        3)
+            sleep 0.3
+            echo "postgresql_menu"
+            ;;
+        4)
+            sleep 0.3
+            echo "sqlite_menu"
+            ;;
+        0)
+            sleep 0.3
+            return
+            ;;
+        *)
+            sleep 0.3
+            echo -e "${WARNING}${BOLD}⚠ AVISO ⚠ ${NC}: Opção inválida!"
+            sleep 0.3
+            database_menu
+            ;;
+    esac
+}
+
 #function apache_menu(){}
 #function nginx_menu(){}
 #function samba_menu(){}
@@ -19,13 +62,15 @@ BLINK='\033[5m'
 #function vsftpd_menu(){}
 function server_menu(){
     echo -e "${NL}${BLUE}########################"
-    echo -e "##     ${NC}${BOLD}SERVIDORES${NC}${BLUE}     ##"
+    echo -e "##       ${NC}${BOLD}SERVIDORES${NC}${BLUE}   ##"
     echo -e "##....................##"
     echo -e "##${NC} [${INPUT}1${NC}] - Apache       ${BLUE}##"
     echo -e "##${NC} [${INPUT}2${NC}] - NginX        ${BLUE}##"
     echo -e "##${NC} [${INPUT}3${NC}] - Samba        ${BLUE}##"
     echo -e "##${NC} [${INPUT}4${NC}] - ProFTPD      ${BLUE}##"
     echo -e "##${NC} [${INPUT}5${NC}] - vsFTPD       ${BLUE}##"
+    echo -e "##${NC} [${INPUT}0${NC}] - Voltar       ${BLUE}##"
+
     echo -e "########################${NC}"
     echo -ne "${BLINK}${INPUT}>>>${NC} Selecione uma opção: "
     read -r server_option
@@ -65,5 +110,5 @@ function server_menu(){
 }
 
 #function main_menu(){}
-
-server_menu
+#server_menu
+#database_menu
